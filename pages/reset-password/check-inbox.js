ae7d7ca3d83acd13853c1239b026f4c1c6fa4118
oluwatimileyin <oluwatimileyinakinpelu49@gmail.com>
2022-02-23 10:49:46 +0100

    reset password/check inbox screen implemented

--- pages/reset-password/check-inbox.js
+++ pages/reset-password/check-inbox.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-import AuthLayouts from '../components/AuthLayouts'
-import Card from '../components/Card'
-import inbox from '../public/inbox.png'
+import AuthLayouts from './../../components/AuthLayouts'
+import inbox from '../../public/inbox.png'
 
 const checkInbox = () => {
   return (
